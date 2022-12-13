93c13e403b060d9b1f1a1cacb0cca042af50e75f
praneethkumar73 <praneethkumar73@gmail.com>
2022-12-13 12:39:04 +0530

    functions

---
+++ hello_world/calculations.py
@@ -0,0 +1,16 @@
+# Calculations
+import math
+
+width = 15
+height = 20
+pi = 3.14
+
+area = width*height
+print(area)
+
+circum = 2*(width*height)
+print(circum)
+
+r = width*width*math.pi
+print(r)
+print("the value of r is", r)
